1e13d87fd0e0604c3d61b3cca414ee4eb0dc2a5b
MikhailoTrubochki <mikhailotrubochki@waifu.club>
2026-08-04 09:48:49 +0000

    feat: add ipload with proxy and timeout support

---
+++ ipload.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+	"time"
+)
+
+func ipload(country string) error {
+	urlStr := fmt.Sprintf("https://scanitex.com/resources/ipdb/ipv4/%s.zone", strings.ToLower(country))
+	path := fmt.Sprintf("data/raw/%s.zone", strings.ToLower(country))
+
+	client := &http.Client{Timeout: 30 * time.Second}
+
+	if proxy := os.Getenv("all_proxy"); proxy != "" {
+		proxyURL, err := url.Parse(proxy)
+		if err != nil {
+			return fmt.Errorf("invalid proxy: %v", err)
+		}
+		client.Transport = &http.Transport{Proxy: http.ProxyURL(proxyURL)}
+	}
+
+	resp, err := client.Get(urlStr)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		return fmt.Errorf("failed to download: %s", resp.Status)
+	}
+
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	if err := os.WriteFile(path, data, 0644); err != nil {
+		return err
+	}
+
+	lines := strings.Count(string(data), "\n")
+	fmt.Printf("downloaded %d IPs to %s\n", lines, path)
+	return nil
+}
--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("hi it Mikhailo Trubochki")
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: rewl <command> [args]")
+		fmt.Println("Commands:")
+		fmt.Println("  ipload <country>  download IP list for country")
+		os.Exit(1)
+	}
+
+	cmd := os.Args[1]
+	switch cmd {
+	case "ipload":
+		if len(os.Args) < 3 {
+			fmt.Println("Usage: rewl ipload <country>")
+			os.Exit(1)
+		}
+		if err := ipload(os.Args[2]); err != nil {
+			fmt.Fprintf(os.Stderr, "error: %v\n", err)
+			os.Exit(1)
+		}
+	default:
+		fmt.Printf("unknown command: %s\n", cmd)
+		os.Exit(1)
+	}
 }
